3dd0769c1f5146a979efc4581790224571d0bca6
Vaibhav262610 <branda56235@gmail.com>
2022-06-20 23:17:01 -0700

    add

--- components/Header.js
+++ components/Header.js
@@ -3,20 +3,24 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <div className="flex flex-col  md:flex-row items-center justify-center gap-10">
+      <div className="flex flex-col ml-6 sm:ml-0  md:flex-row items-center justify-center gap-10">
         <div className="flex flex-col mt-44">
           <div className="flex items-center gap-1">
             <img src="/discount.png" className="h-4" />
-            <h3 className="font-bold">
+            <h3 className=" text-1xl sm:text-3xl">
               GET <span className="text-[#858585]">25%</span> OFF NOW
             </h3>
           </div>
-          <h1 className="mt-2 text-6xl !font-extralight  ">Make your life</h1>
+          <h1 className="mt-2 text-5xl sm:text-6xl !font-extralight  ">
+            Make your life
+          </h1>
           <div className="flex gap-6 items-center">
-            <h1 className="text-6xl ">easier with</h1>
-            <img src="/sparkle.png" className="h-12 mt-3" />
+            <h1 className=" text-5xl sm:text-6xl !font-extralight ">
+              easier with
+            </h1>
+            <img src="/sparkle.png" className="h-12 mt-3 hidden sm:block" />
           </div>
-          <h1 className="text-6xl ">crypto</h1>
+          <h1 className=" text-5xl sm:text-6xl !font-extralight">crypto</h1>
           <button className="mt-12 w-fit font-bold py-2 rounded-lg px-8 bg-green-400">
             Start Now
           </button>
@@ -39,12 +43,28 @@ const Header = () => {
           </div>
         </div>
         <div className="relative">
-          <img src="/bubble1.png" className="absolute mt-12 ml-32 hidden md:block "  width="45"/>
-          <img src="/bubble2.png" className="absolute  mt-12 ml-80 left-28 hidden md:block "  width="35"/>
-          <img src="/bubble1.png" className="absolute mt-72 ml-80 left-56   hidden md:block"  width="45"/>
+          <img
+            src="/bubble1.png"
+            className="absolute mt-12 ml-32 hidden md:block "
+            width="45"
+          />
+          <img
+            src="/bubble2.png"
+            className="absolute  mt-12 ml-80 left-28 hidden md:block "
+            width="35"
+          />
+          <img
+            src="/bubble1.png"
+            className="absolute mt-72 ml-80 left-56   hidden md:block"
+            width="45"
+          />
 
           <div className="pl-10  ">
-            <img src="/main_card.png " className=" animate-wiggle" width="500" />
+            <img
+              src="/main_card.png "
+              className=" animate-wiggle"
+              width="500"
+            />
           </div>
         </div>
       </div>
